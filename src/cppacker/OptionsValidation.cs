36583bcf075dff843675f39e94d00dd042db2c0c
Eric Newton <ericnewton76@gmail.com>
2018-10-17 22:42:37 -0400

    created OptionsValidation (would be in CommandLine package)

---
+++ src/cppacker/OptionsValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cppacker
+{
+	public class OptionsValidation
+	{
+		List<string> messages = new List<string>();
+
+		public bool IsValid { get { return this._IsValid; } }
+		private bool _IsValid;
+
+		public IEnumerable<string> GetMessages()
+		{
+			return messages;
+		}
+
+		public void AddMessage(string optionName, string message, bool isValid = true)
+		{
+			this.messages.Add(optionName + ": " + message);
+			if(isValid == false) this._IsValid = false;
+		}
+
+
+	}
+}
