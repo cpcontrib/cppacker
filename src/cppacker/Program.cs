db6f4100ddc0e4d873425cc8317f32fd00ac0d80
Eric Newton <ericnewton76@gmail.com>
2018-10-17 20:28:47 -0400

    created ProgramOptions

--- src/cppacker/Program.cs
+++ src/cppacker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace cppacker
 	{
 		static void Main(string[] args)
 		{
+			ProgramOptions ProgramOptions = new ProgramOptions() {
+				ProjectFile = @"C:\Projects\cpcontrib\core\src\cpcontrib.core.csproj"
+			};
+
+			if(File.Exists(ProgramOptions.ProjectFile) == false)
+			{
+				Console.WriteLine("projectfile doesnt exist");
+			}
+			else
+			{
+			}
 		}
+
+
 	}
 }
