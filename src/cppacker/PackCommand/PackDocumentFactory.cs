593cb1e7b3975912dcb0348c3bf283c1b2bccaee
Eric Newton <ericnewton76@gmail.com>
2018-12-21 17:39:48 -0500

    [ci skip] added tests project + first draft of PackerDirectiveParser_Tests

---
+++ src/cppacker/PackCommand/PackDocumentFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSG;
+
+namespace cppacker.Pack
+{
+	public class PackDocumentFactory
+	{
+
+		public Document Document;
+
+		public Promise GetSyntaxTree;
+		public Promise GetPackerDirectives;
+
+		public RSG.Promise<PackDocument> Create()
+		{
+			return new Promise<PackDocument>((resolve, reject) => {
+
+				Promise<object>.All((IPromise<object>)GetSyntaxTree, (IPromise<object>)GetPackerDirectives)
+				.Then(results => {
+					resolve(new PackDocument() {
+						Document = Document,
+						SyntaxTree = (SyntaxTree)results.ElementAt(0),
+						PackerDirectives = (IEnumerable<PackerDirective>)results.ElementAt(1)
+					});
+				});
+
+			});
+		}
+
+	}
+}
---
+++ src/cppacker_Tests/PackerDirectiveParser_Tests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using cppacker.Parsing;
+
+namespace cppacker_Tests
+{
+	[TestFixture]
+	public class PackerDirectiveParser_Tests
+	{
+		[Test]
+		[TestCase("//!packer:targetFile=filenamespec.cs", "targetFile", "filenamespec.cs")]
+		[TestCase("//!packer:region=ASDF ASDF", "region", "ASDF ASDF")]
+		[TestCase("//!packer:endregion", "endregion", null)]
+		public void TestMethod(string rawtext, string directiveName, string directiveOptions)
+		{
+			var parser = CreatePackerParser();
+
+			var parsed = parser.ParseLines(new string[] { rawtext })
+				.ToArray();
+
+			Assert.That(parsed[0], Is.TypeOf<ParserDirective>());
+
+
+
+			Assert.That(directive.Name, Is.EqualTo(directiveName));
+			Assert.That(directive.Options, Is.EqualTo(directiveOptions));
+		}
+
+		[Test]
+		[TestCase("//!packer:targetFile=filenamespec.cs;sortOrder=999;asdf=ttt", "targetFile,sortOrder,asdf")]
+		public void DelimitedOptions(string rawtext, string directiveNamesString)
+		{
+			var parser = CreatePackerParser();
+
+			var parsed = parser.ParseLines(new string[] { rawtext })
+				.ToArray();
+
+			Assert.That(parsed[0].Type, Is.TypeOf<ParserDirectives>());
+
+			var directives = parsed
+
+			Assert.That(directive.Name, Is.EqualTo(directiveName));
+			Assert.That(directive.Options, Is.EqualTo(directiveOptions));
+		}
+
+		private PackerDirectiveParser CreatePackerDirectiveParser()
+		{
+			return new PackerDirectiveParser();
+		}
+
+	}
+}
