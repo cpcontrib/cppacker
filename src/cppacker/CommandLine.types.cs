869b642fb7235e1ffce52a28051d016782c035e3
Eric Newton <ericnewton76@gmail.com>
2018-12-28 16:16:41 -0500

    move file

--- src/cppacker/CommandLine.types.cs
+++ src/cppacker/CommandLine.types.cs
@@ -9,29 +9,74 @@ namespace CommandLine
 	public interface ICommand
 	{
 		int Execute();
-		OptionsValidation ValidateOptions(ICommandOptions options);
 	}
-	public interface ICommandOptions
+
+	/// <summary>
+	/// Interface for an command that can do options validation.
+	/// </summary>
+	public interface ICommandOptionsValidator
 	{
+		OptionsValidation Validate();
+		bool IsValid();
 	}
+
+	/// <summary>
+	/// Interface for an options validator.  Normally used for a separate class that validates options.  If your Command class validates directly, use <see cref="ICommandOptionsValidator"/>
+	/// </summary>
+	/// <typeparam name="Toptions"></typeparam>
+	public interface IOptionsValidator<Toptions>
+	{
+		OptionsValidation Validate(Toptions options);
+	}
+
 	public class OptionsValidation
 	{
-		List<string> messages = new List<string>();
+		List<Tuple<bool,string>> messages = new List<Tuple<bool, string>>();
 
 		public bool IsValid { get { return this._IsValid; } }
 		private bool _IsValid = true;
 
 		public IEnumerable<string> GetMessages()
 		{
-			return messages;
+			return messages.Where(_=>_.Item1 == true).Select(_=>_.Item2);
 		}
 
-		public void AddMessage(string optionName, string message, bool isValid = true)
+		public OptionsValidation AddMessage(string optionName, string message, bool isValid = true)
 		{
-			this.messages.Add(optionName + ": " + message);
+			this.messages.Add(new Tuple<bool,string>(isValid, optionName + ": " + message));
 			if(isValid == false) this._IsValid = false;
+			return this;
 		}
 
+		/// <summary>
+		/// Writes any option validation messages to given TextWriter (usually Console.Error/stderr).
+		/// Returns true if any error messages written out.
+		/// </summary>
+		/// <param name="writer"></param>
+		/// <returns></returns>
+		public OptionsValidation WriteWhenNotValid(System.IO.TextWriter writer)
+		{
+			if(IsValid == true) return this;
 
+			var messages = GetMessages();
+
+			if(messages.Count() > 0)
+			{
+				foreach(var message in messages)
+				{
+					writer.WriteLine(message);
+				}
+			}
+
+			return this;
+		}
+
+		public int NonZeroExit()
+		{
+			if(this.IsValid == false)
+				return 255;
+			else
+				return 0;
+		}
 	}
 }
